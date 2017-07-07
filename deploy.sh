657000d6faa1cf4ef4b405bc646cdce08d5c9050
chingtoe365 <vagrant@precise64.(none)>
2017-07-07 12:25:21 +0000

    vote change title

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# bash
+cd ~/semaphore/vote
+sudo docker build -t semaphore/vote .
+if [ ! "$(docker ps -a | grep vote)" ]; then
+	sudo docker stop vote
+fi
+sudo docker run --name vote -d -p 8080:80 --link redis:redis semaphore/vote
